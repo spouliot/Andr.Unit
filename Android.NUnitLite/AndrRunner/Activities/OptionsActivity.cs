5d5b2649f32d623581c05b1a124d7341414d9b84
Sebastien Pouliot <sebastien@xamarin.com>
2011-11-05 10:57:54 -0400

    Simplify OptionsActivity and use the global new Options to store them

--- Android.NUnitLite/AndrRunner/Activities/OptionsActivity.cs
+++ Android.NUnitLite/AndrRunner/Activities/OptionsActivity.cs
@@ -10,66 +10,44 @@ using MonoDroid.Dialog;
 namespace Android.NUnitLite.UI {
 	
 	[Activity (Label = "Options")]			
-	public class OptionsActivity : Activity {
+	public class OptionsActivity : DialogActivity {
 		BooleanElement remote;
-		//StringElement host_name;
 		EntryElement host_name;
-		StringElement host_port;
+		EntryElement host_port;
 		
 		protected override void OnCreate (Bundle bundle)
 		{
-			base.OnCreate (bundle);
-
-			remote = new BooleanElement ("Remote Server", false);
-			// FIXME: EntryElement does not work
-			//host_name = new StringElement ("HostName");
-			host_name = new EntryElement ("HostName", String.Empty);
-			host_port = new StringElement ("Port");
+			Options options = AndroidRunner.Runner.Options;
+			remote = new BooleanElement ("Remote Server", options.EnableNetwork);
+			host_name = new EntryElement ("HostName", options.HostName);
+			host_port = new EntryElement ("Port", options.HostPort.ToString ()) { Numeric = true };
 			
-			if (bundle != null) {
-				remote.Value = bundle.GetBoolean ("remote");
-				host_name.Value = bundle.GetString ("hostName") ?? String.Empty;
-				host_port.Value = bundle.GetInt ("hostPort").ToString ();
-			} else {
-				ISharedPreferences prefs = GetSharedPreferences ("options", FileCreationMode.Private);
-				remote.Value = prefs.GetBoolean ("remote", false);
-				host_name.Value = prefs.GetString ("hostName", String.Empty);
-				host_port.Value = prefs.GetInt ("hostPort", -1).ToString ();
-			}
-			
-			var root = new RootElement ("Options") {
+			Root = new RootElement ("Options") {
 				new Section () { remote, host_name, host_port }
 			};
-
-			var lv = new ListView (this) {
-				Adapter = new DialogAdapter (this, root)
-			};
-			SetContentView (lv);
+			
+			base.OnCreate (bundle);
 		}
 		
-		protected override void OnPause ()
+		int GetPort ()
 		{
-			ISharedPreferences prefs = GetSharedPreferences ("options", FileCreationMode.Private);
-			var edit = prefs.Edit ();
-			edit.PutBoolean ("remote", remote.Value);
-			edit.PutString ("hostName", host_name.Value);
-			int port = -1;
+			int port;
 			ushort p;
 			if (UInt16.TryParse (host_port.Value, out p))
 				port = p;
 			else
 				port = -1;
-			edit.PutInt ("hostPort", port);
-			edit.Commit ();
-			base.OnPause ();
+			return port;
 		}
 		
-		protected override void OnSaveInstanceState (Bundle outState)
+		protected override void OnPause ()
 		{
-			outState.PutBoolean ("remoteServer", remote.Value);
-			outState.PutString ("hostName", "10.0.1.2"); //host_name);
-			outState.PutInt ("hostPort", 16384); //host_port);
-			base.OnSaveInstanceState (outState);
+			Options options = AndroidRunner.Runner.Options;
+			options.EnableNetwork = remote.Value;
+			options.HostName = host_name.Value;
+			options.HostPort = GetPort ();
+			options.Save (this);
+			base.OnPause ();
 		}
 	}
 }
